92823fdd8d2b3659966ac0abbae7050428f6446a
christopher18-cyber <christopheradegoke4@gmail.com>
2026-06-18 16:22:42 +0100

    setting up the repo

--- src/main/java/com/genius/Main.java
+++ src/main/java/com/genius/Main.java
@@ -1,11 +1,23 @@
 package com.genius;
 
+import com.genius.model.Alien;
+import com.genius.model.AlienRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args){
-        SpringApplication.run(Main.class,args);
+        ApplicationContext context = SpringApplication.run(Main.class,args);
+
+        Alien alien1 = context.getBean(Alien.class);
+//        alien1.setId(101);
+//        alien1.setName("Abraham");
+//        alien1.setTech("Java");
+
+        AlienRepo repo = context.getBean(AlienRepo.class);
+        repo.save(alien1);
+        System.out.println(repo.findAll());
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into alien (id, name, tech) values (101, 'Chris', 'AI');
+insert into alien (id,name,tech) values (102,"Tola","Frontend");
+-- insert into alien (id,name,tech) values (103,"Joy","Backend");
